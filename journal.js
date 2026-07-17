3b275a6b30d1d350acb2f3343197d48649cf0d7a
AaradhyaBarnwal <aaradhyabarnwal82@gmail.com>
2026-07-17 22:30:56 +0530

    3 pages almost completed

---
+++ journal.js
@@ -0,0 +1,16 @@
+let i = 0;
+let txt = `So this is an introductory paragraph of this page. In this page you can
+         finding all the  write ups written by me like stories,poems and articles.`;
+let speed = 50;
+let type = document.getElementById("text");
+let card = document.querySelector(".j");
+card.addEventListener("click",()=>{
+    writer();
+})
+function writer() {
+    if(i<txt.length){
+        type.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(writer,speed);
+    }
+}
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let starTag = document.querySelector(".star");
+let stTag = document.querySelector(".st");
 
 starTag.addEventListener("click",()=>{
     window.location.href = "project.html";
+})
+
+stTag.addEventListener("click",()=>{
+    window.location.href = "journal.html";
 })
